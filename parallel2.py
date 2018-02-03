9e87d6d44fccb8081a285e78ad6f0d0aa570a969
Morris Kurz <morriskurz@gmail.com>
2018-02-03 12:02:33 +0100

    Add sparse matrix multiplication.

--- parallel2.py
+++ parallel2.py
@@ -16,6 +16,7 @@ import numpy as np
 from time import time
 import matplotlib.pyplot as plt
 import math
+from scipy.sparse import diags
 
 
 
@@ -28,62 +29,51 @@ def solve(process):
             values: values at step x]
     """
     initial_cond, pipeline, max_delay, process_const = process
-    def fds(points, process_const):
+    def fds(values):
         # Here: simple heat equation with discretization with first-order forward
         # difference in time and second-order central difference in space
-        left, middle, right = points
-        delta_x, delta_t = process_const
-        c = 0
-        # For now, it is constant 0.1
-        temp = alpha*(right - 2*middle + left) / (delta_x**2)
-        temp2 = c*(right - left) / (2*delta_x)
-        return delta_t*(temp-temp2) + middle
+        # For now, delta_t/delta_x/delta_xit is constant 0.1
+        return r_alpha*A*values+values[1:-1]
     tic = time()
     print(tic)
     # Index of current ghost point front
-    index_right = 0
-    index_left = 0
+    index_right = -1
+    index_left = -1
     # Extract information from variables
     resolution = len(initial_cond)
     time_steps, delta_x, delta_t, r_alpha = process_const
+    process_delay = 0
+    # Sparse finite-difference matrix
+    A =  diags([[-2]*resolution, [1]*resolution, [1]*resolution], [1, 0, 2], shape=(resolution, resolution+2))
     # Define the (for the pe) relevant grid with 2 ghost points
-    prev_values = np.zeros((resolution,))
-    new_values = np.zeros((resolution,))
-    ghost_left = np.zeros((time_steps,))
-    ghost_right = np.zeros((time_steps,))
+    prev_values = np.zeros((resolution+2,), dtype=np.float64)
+    new_values = np.zeros((resolution,), dtype=np.float64)
+    ghost_left = np.zeros((time_steps,), dtype=np.float64)
+    ghost_right = np.zeros((time_steps,), dtype=np.float64)
     # Set initial conditions
-    prev_values[:] = initial_cond
-    # Get initial condition on ghost points
-    ghost_left[0] = pipeline[0].recv()
-    ghost_right[0] = pipeline[2].recv()
+    prev_values[1:-1] = initial_cond
     # Start solving loop
     for i in range(1, time_steps+1):
-        for j in range(1, resolution-1):
-            # For inner points, normal finite difference scheme
-            new_values[j] = fds(
-                    (prev_values[j-1], prev_values[j], prev_values[j+1]),
-                    (delta_x, delta_t))
         # Special handling for delay left; block process if max_delay is
         # exceeded.
-
         while (pipeline[0].poll() and index_left < i-1) or (i-index_left>max_delay+1):
             ghost_left[index_left+1] = pipeline[0].recv()
             index_left += 1
-        new_values[0] = fds((ghost_left[index_left], prev_values[0], prev_values[1]), (delta_x, delta_t))
-        pipeline[1].send(new_values[0])
+        prev_values[0] = ghost_left[index_left]
         while (pipeline[2].poll() and index_right < i-1) or (i-index_right>max_delay+1):
             ghost_right[index_right+1] = pipeline[2].recv()
             index_right += 1
-        
-        new_values[-1] = fds((prev_values[-2], prev_values[-1], ghost_right[index_right]), (delta_x, delta_t))
+        prev_values[-1] = ghost_right[index_right]
+        # Save the highest occuring delay.
+        process_delay = max(process_delay, i-index_right-1, i-index_left-1)
+        # Compute fds
+        new_values = fds(prev_values)
+        pipeline[1].send(new_values[0])
         pipeline[3].send(new_values[-1])
         if i == time_steps:
-            print(ghost_left)
-            print(ghost_right)
-            print(prev_values)
-            print(new_values)
+            print("Max delay:", process_delay)
             return (time()-tic, new_values)
-        prev_values = np.array(new_values)
+        prev_values[1:-1] = np.array(new_values)
         
 
 def synchronous(process):
@@ -144,7 +134,7 @@ def plot_results(results):
         plt.loglog(time_results[i, :], grid_results[i, :],
                    colors[delays[i]], label=delay_to_label[delays[i]])
 
-def setup_pipeline(amount_pe, length_pe, initial_cond, resolution):
+def setup_pipeline(amount_pe, pe_ranges, initial_cond, resolution):
     # Using pipelines for communication, each process need 2 receiving
     # pipelines (ghost points) and 2 outgoing pipelines corresponding to
     # adjacent boundary points.
@@ -162,18 +152,14 @@ def setup_pipeline(amount_pe, length_pe, initial_cond, resolution):
          # Analogous to above, just modulo amount_pe to not get IndexError
          pipeline[(i+1)%amount_pe][0] = right_read
          pipeline[i][3] = right_write
-    for i in range(amount_pe):
-        if i == amount_pe-1:
-            # If on the border of the domain, use rightmost point
-            boundary = resolution-1
-        else:
-            # Otherwise use next point
-            boundary = (i+1)*length_pe-1
+    for i, pe_range in enumerate(pe_ranges):  
+        left = pe_range[0]
+        right = pe_range[1]
         # Ghost points for boundary points, here the computated
         # information gets stored
-        pipeline[i][1].send(initial_cond[i*length_pe])
+        pipeline[i][1].send(initial_cond[left])
         #print(pipeline[i-1][2].recv()==initial_cond[i*length_pe])
-        pipeline[i][3].send(initial_cond[boundary])
+        pipeline[i][3].send(initial_cond[right])
     return pipeline
 
 
@@ -192,13 +178,15 @@ def setup_pe_ranges(amount_pe, resolution, length_pe):
         pe_ranges.append((i*length_pe, boundary))
     return pe_ranges
 
-def combine_results(results, amount_pe, resolution):
-    grid = []
+def combine_results(results, amount_pe):
     time = []
+    resolution = sum(map(lambda x: len(x[1]), results))
+    grid = np.zeros((resolution,), dtype=np.float64)
+    index = 0
     for result in results:
         time.append(result[0])
-        grid.append(result[1])
-    grid = np.array(grid).reshape((resolution,))
+        grid[index:index+len(result[1])] = result[1]
+        index += len(result[1])
     residual = np.linalg.norm(
                     exact_solution_end(resolution)-grid, np.inf)
     time = np.mean(time)
@@ -206,15 +194,15 @@ def combine_results(results, amount_pe, resolution):
     
 
 end_time = (2*math.pi)**2/64
-colors = {0: "b-", 1: "g-", 10: "r-", 100: "k-", 1000: "m-", 25000: "c-"}
-delay_to_label = {0: "Synchronous", 1: "Delay 1", 10: "Delay 10", 100: "Delay 100", 1000: "Delay 1000", 25000: "Max. Delay"}
+colors = {0: "b-", 1: "g-", 10: "r-", 100: "k-", 1000: "m-", 1000000: "c-"}
+delay_to_label = {0: "Synchronous", 1: "Delay 1", 10: "Delay 10", 100: "Delay 100", 1000: "Delay 1000", 1000000: "Max. Delay"}
 r_alpha = 0.1
-grid_resolution = [32, 64, 128, 256, 512, 1024]
+grid_resolution = [64]
 all_delta_x = [2*math.pi/r for r in grid_resolution]
 all_delta_t = [r_alpha*x*x for x in all_delta_x]
 all_time_steps = [math.ceil(end_time/delta_t) for delta_t in all_delta_t]
-delays = [0, 1, 1000, 25000]
-amount_pe = 4
+delays = [0, 1000000]
+amount_pe = 8
 wavenumber = 1
 amplitude = 1
 phase_angle = 0.33
@@ -246,13 +234,13 @@ if __name__ == '__main__':
             initial_cond = get_initial_cond(resolution)
             pe_ranges = setup_pe_ranges(amount_pe, resolution, length_pe)
             for j, delay_time in enumerate(delays):
-                pipeline = setup_pipeline(amount_pe, length_pe, initial_cond, resolution)
+                pipeline = setup_pipeline(amount_pe, pe_ranges, initial_cond, resolution)
                 with Pool(amount_pe) as pool:
                     params = [(initial_cond[pe_ranges[i][0]:pe_ranges[i][1]+1],
                                pipeline[i], delay_time,[time_steps, delta_x,
                                         delta_t, r_alpha]) for i in range(amount_pe)]
                     results = pool.map(solve, params, 1)
-                results = combine_results(results, amount_pe, resolution)
+                results = combine_results(results, amount_pe)
                 grid_results[j, i] += results[-1]
                 time_results[j, i] += results[0]
             print("Run", i, "Time:", time()-tic)
@@ -260,4 +248,5 @@ if __name__ == '__main__':
     time_results = time_results/average
     plot_results((grid_results, time_results))
     print(time()-tic)
+    print(time_results)
     plt.legend(loc="upper right")
